123a72b4368a3cf347cfd99aa50f00c9e02adb31
prrao87 <prrao87@gmail.com>
2020-09-29 17:00:45 -0400

    Fix bug: Lowercase required for fasttext

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ st.markdown(f"<h2 style='text-align: center;'><b>{title_text}</b></h2>", unsafe_
 st.markdown(f"<h5 style='text-align: center;'>{subheader_text}</h5>", unsafe_allow_html=True)
 st.text("")
 input_text = st.text_input('Enter your text:', "")
-n_samples = st.text_input('Number of samples to generate for LIME explainer: (For really long input text, go up to 5000)', value=5000)
+n_samples = st.text_input('Number of samples to generate for LIME explainer: (For really long input text, go up to 5000)', value=1000)
 method_list = tuple(label for label, val in METHODS.items())
 method = st.selectbox(
     'Choose classifier:',
@@ -35,7 +35,8 @@ if st.button("Explain Results"):
         exp = explainer(method,
                         path_to_file=METHODS[method]['file'],
                         text=text,
+                        lowercase=METHODS[method]['lowercase'],
                         num_samples=int(n_samples))
         # Display explainer HTML object
-        components.html(exp.as_html(), height=1000)
+        components.html(exp.as_html(), height=800)
 
